3aa25e354517c751a5623c8619245a4b40f3748a
Munish Singla <munishkmr60@gmail.com>
2025-07-18 20:09:47 +0530

    Cleanup some junk

--- wordpress_plugins/ncmaz-faust-core/src/block-group/deprecated.ts
+++
@@ -1,6 +0,0 @@
-const v1 = {};
-const v2 = {};
-
-const deprecated = [v2, v1];
-
-export default deprecated;
--- wordpress_plugins/ncmaz-faust-core/src/contains/site-settings.ts
+++
@@ -1,6 +0,0 @@
-// import from file json
-import siteSettingsJson from "../../site-settings.json";
-
-export const NC_SITE_SETTINGS = siteSettingsJson;
-
-export const IS_DEV = process.env.NODE_ENV === "development";
